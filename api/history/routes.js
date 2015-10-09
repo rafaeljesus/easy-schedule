6caa55d029e6e195654f766631d5ec0d480fb522
rafaeljesus <rafaelljesus86@gmail.com>
2015-10-09 18:59:08 -0300

    Refactor history routes, collection and related specs to new es6 expor syntax

--- api/history/routes.js
+++ api/history/routes.js
@@ -1,14 +1,14 @@
 import koaRouter from 'koa-router'
-import History from './collection'
+import {findByUser} from './collection'
 
 const router = koaRouter()
 
 router.get('/', function* () {
   try {
-    this.body = yield History.findByUser(this.user)
+    this.body = yield findByUser(this.user)
   } catch(err) {
     this.throw(500, err)
   }
 })
 
-module.exports = router
+export default router

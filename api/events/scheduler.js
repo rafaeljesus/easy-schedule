5cd4919cd6ccf62684a5e96915f6db4c6d1f47fa
rafaeljesus <rafaelljesus86@gmail.com>
2015-10-11 12:55:19 -0300

    Refactor onEvent fn callback to a new separated module, making better for test

--- api/events/scheduler.js
+++ api/events/scheduler.js
@@ -1,38 +1,40 @@
 import co from 'co'
-import request from 'co-request'
 import scheduler from 'node-schedule'
-import log from '../../lib/log'
-import utils from '../../lib/utils'
+import handle from './handle'
 import Event from '../events/collection'
 import History from '../history/collection'
+import log from '../../lib/log'
+import utils from '../../lib/utils'
 
 const isPlainObject = utils.isPlainObject
   , runningJobs = {}
 
 const start = () => {
-  co(function* () {
+  return co(function* () {
     let res = yield Event.findAll()
     if (!res || res.length === 0) return
-
     if (isPlainObject(res)) {
       return create(res)
     }
     res.map(create)
-  })
-  .catch(err => log.error('scheduler failed to start', err))
+  }).
+  catch(err => log.error('scheduler failed to start', err))
 }
 
 const cancel = _id => {
   let job = runningJobs[_id]
-  if (job) {
-    job.cancel()
-    job = null
-  }
+  if (!job) return
+  job.cancel()
+  job = null
 }
 
 const create = event => {
-  const cron = event.cron ? event.cron : new Date(event.when)
-    , job = scheduler.scheduleJob(cron, onEvent.bind(null, event))
+  const cron = event.cron ?
+    event.cron :
+    new Date(event.when)
+
+  const fn = handle.bind(null, event)
+  const job = scheduler.scheduleJob(cron, fn)
 
   runningJobs[event._id] = job
   log.info('succesfully scheduled job', event)
@@ -41,22 +43,6 @@ const create = event => {
 const update = (_id, event) => {
   cancel(_id)
   create(event)
-}
-
-const onEvent = event => {
-  return co(function* () {
-    let res = yield request(event.url)
-    let logObj = {
-      statusCode: res.statusCode,
-      body: res.body,
-      headers: res.headers
-    }
-    log.info('succesfully sent cron job request', logObj)
-    logObj.event = event._id
-    logObj.user = event.user
-    logObj.url = event.url
-    return yield History.create(logObj)
-  }).catch(err => log.error('failed to send cron job request', err))
 }
 
 export default {

d08ad493c4bbe772b89d6d7c292deb7f40350cac
rafaeljesus <rafaelljesus86@gmail.com>
2015-10-17 21:03:37 -0300

    Setup apidoc instead of sweagger Setup apidoc for all rest endpoints NOTE: its not been served statically yet

--- api/home/routes.js
+++ api/home/routes.js
@@ -2,10 +2,20 @@ import koaRouter from 'koa-router'
 
 const router = koaRouter()
 
+/**
+ * @api {get} / API Status
+ * @apiGroup Status
+ * @apiSuccess {String} status API status message
+ * @apiSuccessExample {json} Success
+ *    HTTP/1.1 200 OK
+ *    {
+ *      "status": "Easy Schedule API"
+ *    }
+ */
 router.get('/', function* () {
   this.type = 'json'
   this.status = 200
-  this.body = {message: 'Easy Schedule rest api working'}
+  this.body = {status: 'Easy Schedule API'}
 })
 
 export default router
